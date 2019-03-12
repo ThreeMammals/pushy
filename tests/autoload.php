9684499acc1f69ab383a134b29176612b82961bd
Tom Gardham-Pallister <tom.pallister@fourth.com>
2019-03-12 22:16:01 +0000

    composer

--- tests/autoload.php
+++ tests/autoload.php
@@ -10,4 +10,3 @@ WP_Mock::bootstrap();
  * Now we include any plugin files that we need to be able to run the tests. This
  * should be files that define the functions and classes you're going to test.
  */
-// require_once __DIR__ . '../index.php';
