45ef6a1954bd3fe378725d4c1f711c332ee93923
Tom Gardham-Pallister <tom.pallister@fourth.com>
2019-05-31 21:23:38 +0100

    added terraform to create sns topics for pushy

---
+++ infrastructure/terraform/aws/sns.tf
@@ -0,0 +1,95 @@
+resource "aws_sns_topic" "topic" {
+  name = "PostUpdated"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "PostTrashed"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "Revision"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "PostDraft"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "PageUpdated"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "PageDraft"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "MenuItemPublished"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "FuturePageUpdated"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "FuturePostUpdated"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "UnknownPost"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "PostTrashed"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "PostRestored"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "PostDeleted"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "CategoriesUpdated"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "MenuUpdated"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "MenuDeleted"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "MediaUploaded"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "AttachmentUploaded"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "AttachmentDeleted"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "AttachmentUpdated"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "PagePublished"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "PostMetaUpdated"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "TagsUpdated"
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "OptionUpdated"
+}
